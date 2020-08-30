50244a0b22ffa877c28a999bc03121454684a0d7
Rimberse <kerimpltm@gmail.com>
2020-08-30 14:15:01 +0200

    Add solution to exercise 1.3

--- Part 1/course-information/src/index.js
+++ Part 1/course-information/src/index.js
@@ -42,21 +42,26 @@ const Total = props => {
 }
 
 // Main component
-// Parts could eventually be stores in an array, but i didn't really bother with the code optimization
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
 
   return (
     <div>
       <Header courseName={course} />
-      <Content part1Name={part1} part2Name={part2} part3Name={part3} exercises1Count={exercises1} exercises2Count={exercises2} exercises3Count={exercises3} />
-      <Total exercises1Count={exercises1} exercises2Count={exercises2} exercises3Count={exercises3} />
+      <Content part1Name={part1.name} part2Name={part2.name} part3Name={part3.name} exercises1Count={part1.exercises} exercises2Count={part2.exercises} exercises3Count={part3.exercises} />
+      <Total exercises1Count={part1.exercises} exercises2Count={part2.exercises} exercises3Count={part3.exercises} />
     </div>
   );
 }
